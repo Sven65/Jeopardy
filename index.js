f1775632e13534c2d20050fd1e990a11518af227
Sven65 <thormax5@gmail.com>
2018-06-17 11:44:50 +0200

    Add Stuff

---
+++ index.js
@@ -0,0 +1,22 @@
+const app = require('./app')
+//const http = require("http").Server(app)
+
+
+const port = (process.env.PORT || 3000)
+const host = (process.env.HOST || undefined)
+
+const server = app.listen(port, host, () => {
+	// ask server for the actual address and port its listening on
+	const listenAddress = server.address()
+	console.log(`Jeopardy server running on ${listenAddress.address}:${listenAddress.port}` )
+})
+
+const io = require("socket.io")(server)
+
+io.on("connection", socket => {
+	console.log("connexct")
+
+	socket.on("disconnect", () => {
+		console.log("disconnect")
+	})
+})
